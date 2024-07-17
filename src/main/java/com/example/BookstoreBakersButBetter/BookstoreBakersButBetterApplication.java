ff4b47ad2862ab1adf241038627b627cdcb63207
dyannag <dyanna.grey18@gmail.com>
2024-07-17 16:49:05 -0500

    adding project to repo

---
+++ src/main/java/com/example/BookstoreBakersButBetter/BookstoreBakersButBetterApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookstoreBakersButBetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreBakersButBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreBakersButBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookstoreBakersButBetter/BookstoreBakersButBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookstoreBakersButBetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBakersButBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
