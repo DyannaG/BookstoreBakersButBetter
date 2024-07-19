3658118cd2ea6c722f5ca06f05436a4f91168cbf
ella-n-davis <end41405@gmail.com>
2024-07-19 13:26:53 -0500

    collection model + repo for address, jobapplication, and paymentinfo. added empty mongo config file and added some file stuff in application.properties

---
+++ src/main/java/com/example/BookstoreBakersButBetter/config/MongoSSLConfiguration.java
@@ -0,0 +1,4 @@
+package com.example.BookstoreBakersButBetter.config;
+
+public class MongoSSLConfiguration {
+}
---
+++ src/main/java/com/example/BookstoreBakersButBetter/repository/PaymentInfoRepository.java
@@ -0,0 +1,9 @@
+package com.example.BookstoreBakersButBetter.repository;
+
+import com.example.BookstoreBakersButBetter.collection.PaymentInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentInfoRepository extends MongoRepository<PaymentInfo, String> {
+}
