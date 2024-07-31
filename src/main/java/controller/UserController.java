1c5bcccfe70637a11973ac45e30376a3c14c78a9
dyannag <dyanna.grey18@gmail.com>
2024-07-31 13:40:24 -0500

    User changes

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,16 @@
 package controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableMongoRepositories
+@Slf4j
+@RequestMapping("/User")
 public class UserController {
+    @GetMapping
+    public String Userpage(){
+        return "User";
+    }
 }
--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -3,5 +3,9 @@ package repository;
 //import org.springframework.boot.autoconfigure.security.SecurityProperties;
 //import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+
+import collection.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User,Integer> {
 }
